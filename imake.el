19447819b9e27421fff8381873489938978d220e
Jonas Bernoulli <jonas@bernoul.li>
2026-06-01 17:10:20 +0200

    Release version 1.2.7

--- imake.el
+++ imake.el
@@ -6,10 +6,10 @@
 ;; Homepage: https://github.com/tarsius/imake
 ;; Keywords: convenience
 
-;; Package-Version: 1.2.6
+;; Package-Version: 1.2.7
 ;; Package-Requires: (
 ;;     (emacs  "28.1")
-;;     (compat "30.1"))
+;;     (compat "31.0"))
 
 ;; SPDX-License-Identifier: GPL-3.0-or-later
 
